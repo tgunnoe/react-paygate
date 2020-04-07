9c71dcbbd38235d5d3cd820951220ebc07145d33
Taylor Gunnoe <t@gvno.net>
2020-04-07 05:59:12 -0400

    Adjust settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -11,6 +11,7 @@ import Divider from "@material-ui/core/Divider";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import PageBase from "../components/PageBase";
+import Asynchronous from "./AsyncSettings";
 
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -42,8 +43,17 @@ const styles = theme => ({
     marginTop: 30,
     float: "right"
   },
+  divider: {
+    marginTop: 80,
+    marginBottom: 80
+  },
   saveButton: {
     marginLeft: 5
+  },
+  formElement: {
+    marginTop: 20,
+    minWidth: 200,
+    display: 'block'
   },
   container: {
     display: 'flex',
@@ -76,6 +86,7 @@ class Settings extends React.Component {
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDepository = this.handleDepository.bind(this);
   }
   handleSubmit(event){
     event.preventDefault();
@@ -118,7 +129,11 @@ class Settings extends React.Component {
       this.setState({receiverDepository: event.currentTarget.dataset.depository});
 
   }
-
+  handleDepository( event, value ){
+    this.setState({
+      defaultDepository: value.id,
+    });
+  }
   render() {
     //const classes = useStyles();
     var depositories = [];
@@ -134,143 +149,161 @@ class Settings extends React.Component {
       });
 
     return (
-        <PageBase title="Settings" navigation="Settings">
+      <PageBase title="Settings" navigation="Settings">
 
-        <FormControl fullWidth={true}>
-          <InputLabel htmlFor="originator">Depository</InputLabel>
-          <Select
-            inputProps={{
-              name: "depository",
-              id: "depository"
-            }}
-            fullWidth={true}
-            margin="normal"
-            value={this.state.depository}
-            onChange={this.handleInputChange}
-          >
-        {depositories}
-          </Select>
+        <FormControl fullWidth={true} style={styles.formElement} margin="normal">
+          <Asynchronous
+            onChange={this.handleDepository}
+            url="/depositories"
+            id="depositories-selector"
+            label="Select a depository..."
+            variant="outlined"
+            width={400}
+            />
         </FormControl>
-        <br />
-        <br />
-        <br />
+
+        <Divider style={{marginTop: 10, marginBottom: 10}} />
 
         <form onSubmit={this.handleSubmit}>
-        <TextField
-          hintText="Timezone"
-          label="Timezone"
-          name="timezone"
-          fullWidth={true}
-          margin="normal"
-          defaultValue={this.state.timezone}
-          onChange={this.handleInputChange}
-        />
+          <TextField
+            hintText="Timezone"
+            label="Timezone"
+            name="timezone"
+            fullWidth={false}
+            margin="normal"
+            variant="outlined"
+            style={{ width: 400}}
+            defaultValue={this.state.timezone}
+            onChange={this.handleInputChange}
+            />
 
-        <TextField
-        id="time"
-        label="Cutoff Time"
-        type="time"
-        defaultValue="05:00"
-        //className={classes.textField}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        inputProps={{
-          step: 300, // 5 min
-        }}
-        />
-        <br />
-        <br />
-        <br />
-        <TextField
-          hintText="Inbound ACH path"
-          label="Inbound ACH Path"
-          name="inboundPath"
-          fullWidth={true}
-          margin="normal"
-          defaultValue={this.state.inboundPath}
-          onChange={this.handleInputChange}
-        />
-        <TextField
-          hintText="Outbound ACH path"
-          label="Outbound ACH Path"
-          name="outboundPath"
-          fullWidth={true}
-          margin="normal"
-          defaultValue={this.state.outboundPath}
-          onChange={this.handleInputChange}
-        />
-        <TextField
-          hintText="Returned ACH path"
-          label="Returned ACH Path"
-          name="returnPath"
-          fullWidth={true}
-          margin="normal"
-          defaultValue={this.state.returnPath}
-          onChange={this.handleInputChange}
-        />
-        <br />
-        <br />
-        <br />
+          <FormControl fullWidth={true} style={styles.formElement} margin="normal">
+            <TextField
+              id="time"
+              label="Cutoff Time"
+              type="time"
+              defaultValue="05:00"
+              variant="outlined"
+              style={{ width: 200}}
 
-        <TextField
-          hintText="Hostname"
-          label="Hostname:port"
-          inputProps={{
-              name: "hostname",
-              id: "hostnamee"
-          }}
-          fullWidth={true}
-      margin="normal"
-      defaultValue={this.state.hostname}
-          onChange={this.handleInputChange}
-        />
-        <TextField
-          hintText="Username"
-          label="Username"
-          inputProps={{
-              name: "username",
-              id: "username"
-          }}
-          fullWidth={true}
-          margin="normal"
-          defaultValue={this.state.username}
-          onChange={this.handleInputChange}
-        />
-        <TextField
-          hintText="Password"
-          label="Password"
-          inputProps={{
-              name: "password",
-              id: "password"
-          }}
-          fullWidth={true}
-          margin="normal"
-          type="password"
-          defaultValue={this.state.password}
-          onChange={this.handleInputChange}
-      />
-        <br />
-        <br />
-        <Divider />
-        <br />
-        <br />
-        <div style={styles.buttons}>
-          <Link to="/transfers">
-            <Button variant="contained">Cancel</Button>
-          </Link>
+              //className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              inputProps={{
+                step: 300, // 5 min
+              }}
+              />
+          </FormControl>
+          <FormControl fullWidth={true} style={styles.formElement} margin="normal">
+            <TextField
+              hintText="Inbound ACH path"
+              label="Inbound ACH Path"
+              name="inboundPath"
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              style={{ width: 400}}
+              defaultValue={this.state.inboundPath}
+              onChange={this.handleInputChange}
+              />
+          </FormControl>
+          <FormControl fullWidth={true} style={styles.formElement} margin="normal">
+            <TextField
+              hintText="Outbound ACH path"
+              label="Outbound ACH Path"
+              name="outboundPath"
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              style={{ width: 400}}
+              defaultValue={this.state.outboundPath}
+              onChange={this.handleInputChange}
+              />
+          </FormControl>
+          <FormControl fullWidth={true} style={styles.formElement} margin="normal">
+            <TextField
+              hintText="Returned ACH path"
+              label="Returned ACH Path"
+              name="returnPath"
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              style={{ width: 400}}
+              defaultValue={this.state.returnPath}
+              onChange={this.handleInputChange}
+              />
+          </FormControl>
+          <FormControl fullWidth={true} style={styles.formElement} margin="normal">
+            <TextField
+              hintText="Hostname"
+              label="Hostname:port"
+              inputProps={{
+                name: "hostname",
+                id: "hostnamee"
+              }}
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              style={{ width: 400}}
+              defaultValue={this.state.hostname}
+              onChange={this.handleInputChange}
+              />
+          </FormControl>
+          <FormControl fullWidth={true} style={styles.formElement} margin="normal">
+            <TextField
+              hintText="Username"
+              label="Username"
+              inputProps={{
+                name: "username",
+                id: "username"
+              }}
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              style={{ width: 400}}
+              defaultValue={this.state.username}
+              onChange={this.handleInputChange}
+              />
+          </FormControl>
+          <FormControl fullWidth={true} style={styles.formElement} margin="normal">
+            <TextField
+              hintText="Password"
+              label="Password"
+              inputProps={{
+                name: "password",
+                id: "password"
+              }}
+              fullWidth={true}
+              margin="normal"
+              variant="outlined"
+              style={{ width: 400}}
+              type="password"
+              defaultValue={this.state.password}
+              onChange={this.handleInputChange}
+              />
+          </FormControl>
+          <br />
+          <br />
+          <Divider />
+          <br />
+          <br />
+          <div style={styles.buttons}>
+            <Link to="/transfers">
+              <Button variant="contained">Cancel</Button>
+            </Link>
 
-          <Button
-            style={styles.saveButton}
-            variant="contained"
-            type="submit"
-            color="primary"
-          >
-            Save
-          </Button>
-        </div>
-      </form>
-    </PageBase>
+            <Button
+              style={styles.saveButton}
+              variant="contained"
+              type="submit"
+              color="primary"
+              >
+              Save
+            </Button>
+          </div>
+        </form>
+      </PageBase>
   );
     }
 };
